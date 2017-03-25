1bbb99dd6e1a7b66d665a49d173b1fd359b7e475
John McBride <ubuntu@dfc2eabdf236>
2017-03-25 18:58:57 +0000

    Add water problem set

---
+++ pset1/water.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    printf("How many minutes was your shower? ");
+    int minutes = get_int();
+    
+    printf("Minutes: %i\n", minutes);
+    printf("Bottles: %i\n", (192 * minutes)/16);
+}
