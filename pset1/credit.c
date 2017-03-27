da994fecc82f61fff64a6e67a04650b3e163fcea
John McBride <jpmmcbride@gmail.com>
2017-03-27 21:01:16 +0000

    Working version of credit program

--- pset1/credit.c
+++ pset1/credit.c
@@ -1,7 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void returnArr(long long x, int y);
+bool returnArr(long long x, int y);
 
 int main(void)
 {
@@ -10,15 +10,39 @@ int main(void)
     
     if(card < 1000000000000000 && card > 99999999999999) // American Express 
     {
-        returnArr(card, 15);
+        bool truth = returnArr(card, 15);
+        if(truth)
+        {
+            printf("AMERICAN EXPRESS \n");
+        }
+        else 
+        {
+            printf("INVALID \n");
+        }
     }
     else if(card < 10000000000000000 && card > 999999999999999) // MasterCard
     {
-        returnArr(card, 16);
+        bool truth = returnArr(card, 16);
+        if(truth)
+        {
+            printf("MASTERCARD \n");
+        }
+        else 
+        {
+            printf("INVALID \n");
+        }
     }
     else if(card < 10000000000000 && card > 999999999999) // Visa
     {
-        returnArr(card, 13);
+        bool truth = returnArr(card, 13);
+        if(truth)
+        {
+            printf("VISA \n");
+        }
+        else 
+        {
+            printf("INVALID \n");
+        }
     }
     else
     {
@@ -26,7 +50,7 @@ int main(void)
     }
 }
 
-void returnArr(long long x, int y)
+bool returnArr(long long x, int y)
 {
     int count = 0;
     long long card = x;
@@ -40,13 +64,36 @@ void returnArr(long long x, int y)
         count++;
     }
     
-    for(int i = 1; i < y; i+= 2) // PRINT OUT THE ARRAY
+    int addingDig = 0;
+    
+    for(int i = 1; i < y; i += 2) // PRINT OUT THE ARRAY
     {
-        cardArr[i] = cardArr[i] * 2;
+        cardArr[i] = cardArr[i] * 2; //multiply each by 2
         if(cardArr[i] > 9)
         {
+            int oneDigit = (cardArr[i] % 10);
+            int twoDigit = ((cardArr[i] / 10) % 10);
             
+            addingDig += oneDigit;
+            addingDig += twoDigit;
         }
-        printf("%d ", cardArr[i]);
+        else
+        {
+            addingDig += cardArr[i];
+        }
+    }
+    
+    for(int i = 0; i < y; i += 2) //loop through cardArr again for other numbers in card
+    {
+        addingDig += cardArr[i]; //add them to the ongoing count
+    }
+    
+    if((addingDig % 10) != 0)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
     }
 }
