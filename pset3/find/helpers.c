330e0a1c4d00aa9b749ae14edb7d32b790b15f57
John McBride <jpmmcbride@gmail.com>
2017-04-04 23:40:07 +0000

    Update helper.c with serach function, initial fifteen commit

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -15,25 +15,70 @@ bool search(int value, int values[], int n)
 {
     // TODO: implement a searching algorithm
     
+    int maxIndex = (n - 1);
+    int minIndex = 0;
+    
+    
     if(n < 0)
     {
         return false;
     }
-    else
+    
+    while((maxIndex - minIndex) > 1)
     {
-        if(values[(n/2)] == value)
+        int mid = maxIndex - minIndex;
+        
+        if(values[mid] == value)
         {
             return true;
         }
-        else if(values[(n/2)] > value)
+        else if(values[mid] > value)
+        {
+            maxIndex = mid - 1;
+        }
+        else if(values[mid] < value)
+        {
+            minIndex = mid + 1;
+        }
+        
+    }
+    
+    if (maxIndex - minIndex == 1)
+    {
+        if (values[maxIndex] == value)
+        {
+            return true;
+        }
+        else if (values[minIndex] == value)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
 
 /**
  * Sorts array of n values.
  */
+ 
 void sort(int values[], int n)
 {
     // TODO: implement an O(n^2) sorting algorithm
+    int index = 0;
+    
+    for(int i = (n - 1); i < 0; i--)
+    {
+        for(int j = 1; j < i; j++)
+        {
+            if (values[count] < values[j + 1])
+            {
+                
+            }
+        }
+    }
+    
     return;
 }
