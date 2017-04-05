bcc964131af84646c61fde2652af4f2031071cd2
John McBride <jpmmcbride@gmail.com>
2017-04-05 18:59:57 +0000

    Add comments, final working version

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -5,46 +5,43 @@
  */
  
 #include <cs50.h>
-#include <stdio.h>
-
 #include "helpers.h"
 
 /**
- * Returns true if value is in array of n values, else false.
+ * Returns true if value is in sorted array of n values, else false.
  */
 bool search(int value, int values[], int n)
 {
-    // TODO: implement a searching algorithm
-    
+    //set the range for search window. Also to be used to initialize middle of window being searched
     int maxIndex = (n - 1);
     int minIndex = 0;
     
     
-    if(n < 0)
+    if(n < 0) //searched array must be larger than zero
     {
         return false;
     }
     
-    while((maxIndex - minIndex) > 1)
+    while((maxIndex - minIndex) > 1) //while window to search is large enough, contniue to narrow down index of result
     {
         int mid = maxIndex - minIndex;
         
         if(values[mid] == value)
         {
-            return true;
+            return true; //we got lucky this time!
         }
         else if(values[mid] > value)
         {
-            maxIndex = mid - 1;
+            maxIndex = mid - 1; //reset max index, min index remains the same
         }
         else if(values[mid] < value)
         {
-            minIndex = mid + 1;
+            minIndex = mid + 1; //reset min index, max index remains the same
         }
         
     }
     
-    if (maxIndex - minIndex == 1)
+    if (maxIndex - minIndex == 1) //only two values remain so check both. If not ether value, return false
     {
         if (values[maxIndex] == value)
         {
@@ -60,8 +57,9 @@ bool search(int value, int values[], int n)
         }
     }
     
-    return false;
+    return false; //default case
 }
+
 
 /**
  * Sorts array of n values.
@@ -69,25 +67,21 @@ bool search(int value, int values[], int n)
  
 void sort(int values[], int n)
 {
-    // TODO: implement an O(n^2) sorting algorithm
-    
-        for(int i = n; i > 0; i--)
+    // loop over every element of the array to buuble up largest ints
+    for(int i = n; i > 0; i--)
+    {
+        for(int bubble = 0, j = 1; j < i; j++, bubble++) //second loop for each pair checking if bubble value is larger than test value
         {
+            //set first and second value of bubble pair
+            int bubVal = values[bubble]; 
+            int testVal = values[j]; 
             
-            printf("Im in the first loop, n is %d\n", n);
-            
-            for(int bubble = 0, j = 1; j < i; j++, bubble++)
+            if (values[bubble] > values[j])
             {
-                int bubVal = values[bubble];
-                int testVal = values[j];
-                
-                if (values[bubble] > values[j])
-                {
-                    printf("In the sort deepest loop\n");
-                
-                    values[j] = bubVal;
-                    values[bubble] = testVal;
-                }
+                //if first value in pair is larger, rearrange their positions in the array. Otherwise, continue through loops and on to next bubble pair
+                values[j] = bubVal;
+                values[bubble] = testVal;
             }
         }
+    }
 }
