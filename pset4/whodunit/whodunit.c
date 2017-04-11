df63bd43d0ce1475a9e5648c93c4c7631f1866f0
John McBride <jpmmcbride@gmail.com>
2017-04-11 03:28:31 +0000

    Add command line arguments and file pointers

--- pset4/whodunit/whodunit.c
+++ pset4/whodunit/whodunit.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
+    //ensure proper usage, throws this error if 2 command line arguments are not provided
+    if(argc != 3)
+    {
+        fprintf(stderr, "usage: ./whodunit input_file.bmp output_file.bmp\n");
+        return 1;
+    }
     
+    //remember the locations of the two file names in memory
+    char *infile = argv[1];
+    char *outfile = argv[2];
+    
+    //testing purposes / understand pointers
+    // printf("%s \n", infile);
+    // printf("%i \n", *outfile);
+    
+    return 0;
 }
