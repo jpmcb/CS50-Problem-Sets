22360092892f2cb1092a8bb3bc284edc42e0d2e1
John McBride <jpmmcbride@gmail.com>
2017-04-12 00:18:28 +0000

    Add file opening functions

--- pset4/whodunit/whodunit.c
+++ pset4/whodunit/whodunit.c
@@ -13,6 +13,21 @@ int main(int argc, char *argv[])
     char *infile = argv[1];
     char *outfile = argv[2];
     
+    //open the input file
+    FILE *input = fopen(infile, "r");
+    if(input == NULL) //unable to open the input file
+    {
+        fprintf(stderr, "Could not open %s, please check the file \n", infile);
+        return 2;
+    }
+    FILE *output = fopen(outfile, "w");
+    if(output == NULL)
+    {
+        fclose(input);
+        fprintf(stderr, "Could not write %s, please check your syntax \n", outfile);
+        return 3
+    }
+    
     //testing purposes / understand pointers
     // printf("%s \n", infile);
     // printf("%i \n", *outfile);
